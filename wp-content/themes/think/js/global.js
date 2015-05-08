a8415e04f84a61b1717256064c20e84161a0c8ca
Louis Smith <louis@ahoia.com>
2015-05-08 11:25:23 +0100

    google test 6

--- wp-content/themes/think/js/global.js
+++ wp-content/themes/think/js/global.js
@@ -16,25 +16,9 @@ jQuery(document).ready(function($){
 
 	$('#google-test').on('click', function() {
 	  ga('send', 'event', 'google-test', 'click', 'nav-buttons');
-	});
 
-	var downloadLink = document.getElementById('#google-test');
-	addListener(downloadLink, 'click', function() {
-	  ga('send', 'Videos', 'google-test', 'click', 'nav-buttons');
+	  console.log("clicked button");
 	});
-
-	/**
- * Utility to wrap the different behaviors between W3C-compliant browsers
- * and IE when adding event handlers.
- *
- * @param {Object} element Object on which to attach the event listener.
- * @param {string} type A string representing the event type to listen for
- *     (e.g. load, click, etc.).
- * @param {function()} callback The function that receives the notification.
- */
-function addListener(element, type, callback) {
- if (element.addEventListener) element.addEventListener(type, callback);
- else if (element.attachEvent) element.attachEvent('on' + type, callback);
 
 
 	//mobile nav show the links to scroll down the page
